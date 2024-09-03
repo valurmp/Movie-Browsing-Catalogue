20ba8bfa892ed5ab1eec91528de95dc238d13990
valurmp <valur@agrdynamics.com>
2024-09-03 20:31:39 +0000

    Update Application.java

--- src/main/java/com/team18/MBC/Application.java
+++ src/main/java/com/team18/MBC/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	//jdv4@hi.is was here
+	//vmp2@hi.is was here
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
