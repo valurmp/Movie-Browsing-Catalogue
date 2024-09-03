2bebbe1f2a850af368ceffc245af596b8b36f4c4
kobbi1 <jakob-daniel@hotmail.com>
2024-09-03 20:16:11 +0000

    Initial commit
    Initial commit of the skeleton for our Movie Browsing Catalogue

---
+++ src/main/java/com/team18/MBC/Application.java
@@ -0,0 +1,13 @@
+package com.team18.MBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	//jdv4@hi.is was here
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
