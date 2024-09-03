eb9233ec70011cdaf428bde07f29cc82f5a7c98b
OmarAltabbaa <omar.altabbaa2000@gmail.com>
2024-09-03 21:54:55 +0000

    Update Application.java

--- src/main/java/com/team18/MBC/Application.java
+++ src/main/java/com/team18/MBC/Application.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	//jdv4@hi.is was here
-	//vmp2@hi.is was here
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    //jdv4@hi.is was here
+    //vmp2@hi.is was here
+    //oma9@hi.is was here
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
