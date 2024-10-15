751cef042680fa5df7bd5f45f4d1c0b3974339b8
kobbi1 <jakob-daniel@hotmail.com>
2024-10-15 18:47:45 +0000

    Update MovieRepository.java
    The movierepository can now filter by type

--- src/main/java/com/team18/MBC/core/MovieRepository.java
+++ src/main/java/com/team18/MBC/core/MovieRepository.java
@@ -2,5 +2,10 @@ package com.team18.MBC.core;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByType(String type);
+    Optional<Movie> findByIdAndType(Long id, String type);
 }
