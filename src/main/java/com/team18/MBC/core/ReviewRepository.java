95cbe3a9fbf591f8031fb5e9fd0c15e0491a64dd
OmarAltabbaa <omar.altabbaa2000@gmail.com>
2024-10-16 00:36:09 +0000

    Update ReviewRepository.java
    comment out line to make it not stop running

--- src/main/java/com/team18/MBC/core/ReviewRepository.java
+++ src/main/java/com/team18/MBC/core/ReviewRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByReviewId(Long id);
+  //  List<Review> findByReviewId(Long id);
     List<Review> findAll();
 
 }
