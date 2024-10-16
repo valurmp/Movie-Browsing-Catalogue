77cae461160ee8ab927fb66afe9b00ccbcbdbdaf
kobbi1 <jakob-daniel@hotmail.com>
2024-10-16 22:58:57 +0000

    Modified Controllers for frontend
    Modified MovieController & TvShowController to be able to use it in front end instead of directly displaying the API.

--- src/main/java/Controllers/TvShowController.java
+++ src/main/java/Controllers/TvShowController.java
@@ -2,16 +2,16 @@ package Controllers;
 
 import com.team18.MBC.core.Movie;
 import com.team18.MBC.core.MovieService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
-@RestController
+@Controller
 @RequestMapping("/tvshows")
 public class TvShowController {
     private MovieService movieService;
@@ -20,13 +20,23 @@ public class TvShowController {
         this.movieService = movieService;
     }
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllTvShows() {
+    public String getAllTvShows(Model model) {
         List<Movie> tvShows = movieService.getAllTvShows();
-        return ResponseEntity.ok(tvShows);
+        model.addAttribute("movies", tvShows);
+        model.addAttribute("contextPath", "tvshows");
+        model.addAttribute("contentTitle", "TV Shows");
+        return "movies";
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Movie> getTvShowById(@PathVariable Long id) {
+    public String getTvShowById(@PathVariable Long id, Model model) {
         Movie tvShow = movieService.getTvShowById(id);
-        return tvShow != null ? ResponseEntity.ok(tvShow) : ResponseEntity.notFound().build();
+        if (tvShow != null) {
+            model.addAttribute("movie", tvShow);
+            model.addAttribute("contextPath", "tvshows");
+            return "movie-details";
+        } else {
+            return "404";
+        }
     }
 }
