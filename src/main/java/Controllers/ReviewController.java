093a8369d0d99c2205511c45d1f7dcd7baa6b4d3
OmarAltabbaa <omar.altabbaa2000@gmail.com>
2024-10-16 00:01:15 +0000

    Create ReviewController.java
    Created ReviewControllar

---
+++ src/main/java/Controllers/ReviewController.java
@@ -0,0 +1,28 @@
+package Controllers;
+
+import com.team18.MBC.core.Review;
+import com.team18.MBC.core.ReviewService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    private ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Review>> getAllReviews() {
+        List<Review> reviews = reviewService.getAllReviews();
+        return ResponseEntity.ok(reviews); // 200 OK
+    }
+
+
+}
