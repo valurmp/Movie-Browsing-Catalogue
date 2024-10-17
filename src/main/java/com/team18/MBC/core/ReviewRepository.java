8377d71fa920fac80c616c6a19c54e188294f52d
OmarAltabbaa <omar.altabbaa2000@gmail.com>
2024-10-17 00:39:54 +0000

    Update ReviewRepository.java
     added findById to ReviewRepository

--- src/main/java/com/team18/MBC/core/ReviewRepository.java
+++ src/main/java/com/team18/MBC/core/ReviewRepository.java
@@ -3,9 +3,12 @@ package com.team18.MBC.core;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
   //  List<Review> findByReviewId(Long id);
     List<Review> findAll();
+    Optional<Review> findById(Long id);
+
 
 }
