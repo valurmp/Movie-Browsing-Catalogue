86f4ad8f8eb3c794bdd0d6dbcf483c163af8ad22
kobbi1 <jakob-daniel@hotmail.com>
2024-11-06 17:48:57 +0000

    Added genre retrieval for TV shows
    Updated the MovieRepository to be able fetch genres from a type tvshow.

--- src/main/java/com/team18/MBC/core/MovieRepository.java
+++ src/main/java/com/team18/MBC/core/MovieRepository.java
@@ -1,6 +1,7 @@
 package com.team18.MBC.core;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,4 +9,7 @@ import java.util.Optional;
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     List<Movie> findByType(String type);
     Optional<Movie> findByIdAndType(Long id, String type);
+
+    @Query("SELECT DISTINCT m.genre FROM Movie m WHERE m.type = :type")
+    List<String> findDistinctGenresByType(String type);
 }
