552adcdb52f5b8003f4189d76fdf91e4e7c43890
valurmp <valur@agrdynamics.com>
2024-11-09 23:39:12 +0000

    2 medium endpoints

--- src/main/java/com/team18/MBC/core/MovieRepository.java
+++ src/main/java/com/team18/MBC/core/MovieRepository.java
@@ -1,5 +1,6 @@
 package com.team18.MBC.core;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -17,4 +18,13 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query("SELECT m FROM Movie m WHERE m.genre LIKE %:genre% AND m.type = 'movie'")
     List<Movie> findMovieByGenreContaining(String genre);
+
+    @Query(value = "SELECT m.title, m.genre, m.director, m.releaseYear, m.description, AVG(r.rating) as rating " +
+            "FROM movies m JOIN reviews r ON m.id = r.movie_id " +
+            "WHERE m.type = 'movie' " +
+            "GROUP BY m.id " +
+            "ORDER BY rating DESC " +
+            "LIMIT 10", nativeQuery = true)
+    List<Object[]> getTopMovies();
+
 }
