ef72abb4cd4efec118850a11b3cc3297b958afb0
kobbi1 <jakob-daniel@hotmail.com>
2024-11-19 21:02:07 +0000

    MovieActor class & table created

---
+++ src/main/java/com/team18/MBC/core/MovieActor.java
@@ -0,0 +1,36 @@
+package com.team18.MBC.core;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movie_actor")
+public class MovieActor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", referencedColumnName = "id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "actor_id", referencedColumnName = "id")
+    private Actor actor;
+
+    // Getters and Setters
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+}
